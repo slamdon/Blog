03c187b304712d48a8bd866e4c100d932f9275ae
slamdon <knirosoft@hotmail.com>
2017-01-07 10:47:11 +0800

    add flyweight pattern

---
+++ Design Patterns/Flyweight Pattern/Flyweight Pattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flyweight Pattern
+//
+//  Created by Don on 2017/1/7.
+//  Copyright © 2017年 Don. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
