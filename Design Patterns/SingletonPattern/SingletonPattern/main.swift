bcd33f87ab8503af7280cd06b0d00037b655e16f
slamdon <knirosoft@hotmail.com>
2017-01-20 18:38:05 +0800

    add Proxy Pattern

---
+++ Design Patterns/SingletonPattern/SingletonPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SingletonPattern
+//
+//  Created by don chen on 2017/1/19.
+//  Copyright © 2017年 don chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
